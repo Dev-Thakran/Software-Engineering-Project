77c1ce2a110bc5edad804f0d1a80bbeaeb01905b
Dev-Thakran <devthakran27@gmail.com>
2026-04-07 18:10:29 +1200

    removed the react and other packages and just added the basic folders

--- backend/server.ts
+++
@@ -1,4 +0,0 @@
-import app from "./src/app.ts";
-
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- backend/src/routes/authRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { login } from "../controllers/authController.ts";
-
-const router = Router();
-
-router.post("/login", login);
-
-export default router;
