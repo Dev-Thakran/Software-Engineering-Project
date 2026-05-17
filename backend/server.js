dfe578b968b7cba2efcdf8a7bdfe65592f303705
Dev-Thakran <devthakran27@gmail.com>
2026-05-18 11:21:46 +1200

    staff and manager dashboards done

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const shiftRoutes = require("./routes/shift.routes");
 const checkinRoutes = require('./routes/checkin.routes')
+const managerRoutes = require('./routes/manager.routes')
+app.use('/manager', managerRoutes)
 app.use('/checkins', checkinRoutes)
 app.use("/shifts", shiftRoutes);
 // Parse incoming JSON request bodies
